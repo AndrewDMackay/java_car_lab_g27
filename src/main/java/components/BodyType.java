686fec1c19e6a9f77e420b432dde09a3f9714872
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-16 14:02:03 +0000

    AM, 4th commit main

--- src/main/java/components/BodyType.java
+++ src/main/java/components/BodyType.java
@@ -3,6 +3,18 @@ package components;
 
 import behaviours.IParts;
 
-public enum BodyType implements IParts {
+public enum BodyType {
 
+    MOTORBIKE(0, 2),
+    TWODOOR(2, 4),
+    FOURDOOR(4, 4);
+
+
+    private final int doors;
+    private final int tyres;
+
+    BodyType(int doors, int tyres){
+        this.doors = doors;
+        this.tyres = tyres;
+    }
 }
